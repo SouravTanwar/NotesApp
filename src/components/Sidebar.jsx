d5029e5490cfce09af9e973255302fd0de767c6f
Sourav Tanwar <souravtanwar30@gmail.com>
2024-10-06 17:36:58 +0530

    add: sidebar Component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar({ topics, selectedTopicId, onSelectTopic, onAddTopic }) {
+  // Function to extract initials from topic name
+  const getInitials = (name) => {
+    const initials = name
+      .split(' ')
+      .map(word => word[0])
+      .join('')
+      .toUpperCase();
+    return initials;
+  };
+
+  return (
+    <div className="sidebar">
+      <h2>Pocket Notes</h2>
+      <div className="topics-list">
+        {topics.map((topic) => (
+          <div
+            key={topic.id}
+            className={`topic-item ${topic.id === selectedTopicId ? 'active' : ''}`}
+            onClick={() => onSelectTopic(topic.id)}
+          >
+            <div
+              className="topic-initials"
+              style={{ backgroundColor: topic.color }}
+            >
+              {getInitials(topic.name)}
+            </div>
+            <span className="topic-name">{topic.name}</span>
+          </div>
+        ))}
+      </div>
+      <button className="add-topic-button" onClick={onAddTopic}>
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Sidebar;
